6650e27fdf626a594e2f2cea4fc2fddd038df54a
Amy Bakameyer <bymak87@gmail.com>
2014-09-18 13:46:30 -0500

    first commit

---
+++ formCtrl.js
@@ -0,0 +1,14 @@
+app.controller("formCtrl", function($scope){
+  $scope.student = {
+    firstName: "First",
+    lastName: "Last",
+    mathGrade: "0",
+    scienceGrade: "0",
+    englishGrade: "0",
+    fullName: function () {
+      var x = $scope.student;
+      return x.firstName + " " + x.lastName;
+    }
+  };
+
+});
